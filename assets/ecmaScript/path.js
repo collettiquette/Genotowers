985dd95e923e9a692399051387b81166a91b3054
CumbersomeFugly <cumbersomefugly@gmail.com>
2014-12-27 20:49:55 -0600

    Added genotower.path.translateGrid().

--- assets/ecmaScript/path.js
+++ assets/ecmaScript/path.js
@@ -1,15 +1,18 @@
 genotower.path = {
     
-    convertGrid : function (oldGrid) {
+    translateGrid : function (oldGrid) {
         var x = 0,
-            y = 0;
+            y = 0,
+            newGrid = [];
 
         for (x = 0; x < genotower.config.MAP_WIDTH; x += 1) {
+            newGrid.push([]);
             
             for (y = 0; y < genotower.config.MAP_HEIGHT; y += 1) {
-                
+                newGrid[x].push(Number(oldGrid[x][y].impassable));
             }
         }
+        return newGrid;
     },
     
     checkPath: function () {
