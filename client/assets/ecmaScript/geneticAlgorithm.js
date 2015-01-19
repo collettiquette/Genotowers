faca34aa9cfee3a212064f992dd617626a8f55ae
Kevin Collette <kevcollette@gmail.com>
2015-01-18 23:33:20 -0600

    Remove checking and reverting from evolve.

--- client/assets/ecmaScript/geneticAlgorithm.js
+++ client/assets/ecmaScript/geneticAlgorithm.js
@@ -2,21 +2,8 @@ genotower.geneticAlgorithm = {
 
 
     evolve : function () {
-      var bestTiles = genotower.map.tiles.slice(),
-          bestScore = genotower.generation.scoreFitness(),
-          bestTowers = genotower.towers.slice(),
-          bestWalls = genotower.walls.slice();
-
       genotower.generation.mutateGenome(genotower.towers);
       genotower.generation.mutateGenome(genotower.walls);
       genotower.map.draw();
-
-      if ( genotower.generation.scoreFitness() < bestScore ) {
-        genotower.map.tiles = bestTiles;
-        genotower.towers = bestTowers;
-        genotower.walls = bestWalls;
-        genotower.map.draw();
-      }
-
     }
 };
