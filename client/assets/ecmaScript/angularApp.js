de94cfe0c6b3d60b6499dd58b6fc2623cd9c88ec
rogueSnake <ekanseugor@gmail.com>
2015-01-22 20:14:59 -0600

    Button now disables itself, preventing hilarious bugs.

--- client/assets/ecmaScript/angularApp.js
+++ client/assets/ecmaScript/angularApp.js
@@ -2,10 +2,14 @@ genotower.angularApp = angular.module("genotowerApp", []);
 
 genotower.angularApp.controller("genotowerCtrl", function ($scope) {
 
+    $scope.buttonText = "Start Game"
+
     $scope.mutationRate = 10;
 
     $scope.startGame = function () {
         genotower.config.MUTATION_RATE = ($scope.mutationRate / 100);
+        $scope.buttonText = "Evolution in progress...";
+        $scope.startGame = function () {};
         genotower.run.start();
     };
 });
