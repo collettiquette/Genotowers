09ad7325d7a348fb5b53a2f992daac5dc2d7e856
rogueSnake <ekanseugor@gmail.com>
2015-01-22 21:17:36 -0600

    Added multiple user settings, but failed to set custom max values from within ng-repeat. This means the user can introduce horrible problems such as spawning 200 obstacles on a 100 square grid.

--- client/assets/ecmaScript/angularApp.js
+++ client/assets/ecmaScript/angularApp.js
@@ -1,15 +1,48 @@
 genotower.angularApp = angular.module("genotowerApp", []);
 
 genotower.angularApp.controller("genotowerCtrl", function ($scope) {
+    var i = 0;
 
-    $scope.buttonText = "Start Game"
-
-    $scope.mutationRate = 10;
+    $scope.buttonText = "Start Game";
+    $scope.userSettings = [
+        {
+            title: "Amount of Towers",
+            tag: "towerAmount",
+            value: 10,
+            min: 0,
+            max: 30
+        },
+        {
+            title: "Amount of Walls",
+            tag: "wallAmount",
+            value: 30,
+            min: 0,
+            max: 50
+        },
+        {
+            title: "Mutation Percentage",
+            tag: "mutationPercentage",
+            value: 10,
+            min: 0,
+            max: 100
+        },
+        {
+            title: "Degree of Mutation",
+            tag: "mutationDegree",
+            value: 2,
+            min: 0,
+            max: 10
+        }
+    ];
 
     $scope.startGame = function () {
-        genotower.config.MUTATION_RATE = ($scope.mutationRate / 100);
-        $scope.buttonText = "Evolution in progress...";
+        genotower.config.TOWER_AMOUNT = ($scope.userSettings[0].value);
+        genotower.config.WALL_AMOUNT = ($scope.userSettings[1].value);
+        genotower.config.MUTATION_RATE = ($scope.userSettings[2].value / 100);
+        genotower.config.MUTATION_DEGREE = ($scope.userSettings[3].value);
+
         $scope.startGame = function () {};
+        $scope.buttonText = "Evolution in progress...";
         genotower.run.start();
     };
 });
