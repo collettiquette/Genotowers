38258da4e95d28f28ae4772ae2008b100df3bac8
rogueSnake <ekanseugor@gmail.com>
2015-02-10 00:15:09 -0600

    Changed a variable name and some whitespace.

--- client/assets/ecmaScript/run.js
+++ client/assets/ecmaScript/run.js
@@ -10,39 +10,39 @@ genotower.run = {
     preload : function () {
         var images = [
                 {
-                    key: 'tower',
-                    place: genotower.config.TOWER_IMAGE
+                    key : 'tower',
+                    filePath : genotower.config.TOWER_IMAGE
                 },
                 {
-                    key: 'wall',
-                    place: genotower.config.WALL_IMAGE
+                    key : 'wall',
+                    filePath : genotower.config.WALL_IMAGE
                 },
                 {
-                    key: 'floor',
-                    place: genotower.config.FLOOR_IMAGE
+                    key : 'floor',
+                    filePath : genotower.config.FLOOR_IMAGE
                 },
                 {
-                    key: 'torch',
-                    place: genotower.config.TORCH_IMAGE
+                    key : 'torch',
+                    filePath : genotower.config.TORCH_IMAGE
                 },
                 {
-                    key: 'waypoint',
-                    place: genotower.config.WAYPOINT_IMAGE
+                    key : 'waypoint',
+                    filePath : genotower.config.WAYPOINT_IMAGE
                 },
                 {
-                    key: 'monster',
-                    place: genotower.config.MONSTER_IMAGE
+                    key : 'monster',
+                    filePath : genotower.config.MONSTER_IMAGE
                 },
                 {
-                    key: 'monsterWounded',
-                    place: genotower.config.MONSTER_WOUNDED_IMAGE
+                    key : 'monsterWounded',
+                    filePath : genotower.config.MONSTER_WOUNDED_IMAGE
                 }
             ],
             i = 0,
             max = images.length;
 
         for (i = 0; i < max; i += 1) {
-            genotower.run.game.load.image(images[i].key, images[i].place);
+            genotower.run.game.load.image(images[i].key, images[i].filePath);
         }
     },
 
