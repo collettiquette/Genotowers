ac932c11a1219ebaa34447d564139d7369d7b390
rogueSnake <ekansEugor.gmail.com>
2015-08-08 05:41:11 -0500

    Made target directory.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express(),
     http = require("http").Server(app);
 
-app.use(express.static("client"));
+app.use(express.static("client/target"));
 
 http.listen(3000, function(){
     console.log("Listenin' to port 3000, boss.");
